25f43105b71fd5b7ebb4bf0de74db566a95e4da7
vicTroiano <92123389+VickyTroiano@users.noreply.github.com>
2022-11-27 22:22:42 -0300

    creacion de alumno.json y creacion de variable JSONObj

---
+++ main.js
@@ -0,0 +1,5 @@
+var JSONObj = {};
+var JSONObj = new Object();
+var JSONObj = { "nombreAlumno":"Victoria", "anioMatriculacion":2022 };
+
+console.log(JSONObj.nombreAlumno, "tiene una matricula del año", JSONObj.anioMatriculacion);
