d9e7f475799c44e632306d3203d575a60434ac3b
vicTroiano <92123389+VickyTroiano@users.noreply.github.com>
2022-11-27 23:45:43 -0300

    prueba1

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var JSONObj = {};
-var JSONObj = new Object();
-var JSONObj = { "nombreAlumno":"Victoria", "anioMatriculacion":2022 };
 
-console.log(JSONObj.nombreAlumno, "tiene una matricula del año", JSONObj.anioMatriculacion);
+const deportistas= {
+    nombre: 'Messi',
+    champions: 4
+};
+
+console.log("Hola")
